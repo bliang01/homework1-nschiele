f30b236fd78387bea1b919fe19ef4d2afdb679db
Nathan Schiele <nschiele@uw.edu>
2016-04-16 18:42:10 +0000

    Gradient Descent Completed

--- homework1/exercise2.py
+++ homework1/exercise2.py
@@ -1,6 +1,18 @@
-
 def gradient_step(xk, df, sigma):
-    pass
+    """Returns a step x_k+1 in the gradient descent method
+
+        Parameters
+    ----------
+    xf: float
+    df : function
+    sigma : float
+    
+    Returns
+    -------
+    float
+        The next step of a gradient descent
+    """
+    return xk - sigma * df(xk)
 
 def gradient_descent(f, df, x, sigma=0.5, epsilon=1e-8):
     """Returns a minima of `f` using the Gradient Descent method.
@@ -11,6 +23,27 @@ def gradient_descent(f, df, x, sigma=0.5, epsilon=1e-8):
     `gradient_descent` raises a ValueError if `sigma` is not strictly between
     zero and one.
 
-    [STUDENTS: FILL IN THE REST OF THE DOCUMENTATION!]
+        Parameters
+    ----------
+    f : function
+    df : function
+    x : float
+    sigma : float
+    epsilon : float
+    
+    Returns
+    -------
+    float
+        The local min of f
     """
-    pass
+    
+    if sigma <= 0 or sigma >= 1:
+        raise ValueError('Negative Values not allowed for Collatz')
+    
+    xk = x+1;
+    xk1 = x
+    while abs(xk1 - xk) > epsilon:
+        xk = xk1;
+        xk1 = gradient_step(xk, df, sigma)
+    
+    return xk1
